3571d32f96222cb83cbeca6f00fb42dacaa66dd6
faustouribe <faustouribe@gmail.com>
2023-04-06 12:47:55 -0500

    added local variables

--- mongodb.sh
+++ mongodb.sh
@@ -3,7 +3,7 @@ source local
 docker run -d \
 --name $MONGOSERVERNAME \
 --network cryptonetwork \
--v $MONGODBDATAPATH:/data/db \
+-v $DATAPATH/mongo:/data/db \
 -p 27017:27017 \
 -e MONGO_INITDB_ROOT_USERNAME=$ADMINUSER \
 -e MONGO_INITDB_ROOT_PASSWORD=$ADMINPWD \
