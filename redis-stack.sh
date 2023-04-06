24aa1151c9f4059aad4ddaca7bd9fc841536d4ec
faustouribe <faustouribe@gmail.com>
2023-04-06 12:05:16 -0500

    added restart

--- redis-stack.sh
+++ redis-stack.sh
@@ -1,8 +1,9 @@
 docker run -d \
 --name redis \
+--network cryptonetwork \
 -p 6379:6379 \
 -p 8001:8001 \
 -v /home/mxxxx/redis/data:/data \
 -v /home/mxxxx/redis/local-redis-stack.conf:/redis-stack.conf \
---network cryptonetwork \
+--restart unless-stopped \
 redis/redis-stack:latest
