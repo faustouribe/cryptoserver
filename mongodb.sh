beccb12584b2b078492b9586666096ecb4f8e811
faustouribe <faustouribe@gmail.com>
2023-04-05 19:30:19 -0500

    Added Network

--- mongodb.sh
+++ mongodb.sh
@@ -1,6 +1,7 @@
 docker run -d \
 --name mongodb \
--v /home/xxxxx/mongo:/data/db \
+--network cryptonetwork \
+-v /home/mxxxx/mongo:/data/db \
 -p 27017:27017 \
 -e MONGO_INITDB_ROOT_USERNAME=xxxxx \
 -e MONGO_INITDB_ROOT_PASSWORD=xxxxx \
